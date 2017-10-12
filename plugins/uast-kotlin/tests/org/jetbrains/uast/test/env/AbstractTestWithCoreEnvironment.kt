5ce12e11304df78df44f8ce6fd9961ba24e5d3fc
Nicolay Mitropolsky <Nicolay.Mitropolsky@jetbrains.com>
2017-10-12 19:22:18 +0300

    Revert "[draft] AbstractTestWithCoreEnvironment: `UastMetaLanguage` added"
    This reverts commit 96e145b

--- plugins/uast-kotlin/tests/org/jetbrains/uast/test/env/AbstractTestWithCoreEnvironment.kt
+++ plugins/uast-kotlin/tests/org/jetbrains/uast/test/env/AbstractTestWithCoreEnvironment.kt
@@ -17,14 +17,12 @@
 package org.jetbrains.uast.test.env
 
 import com.intellij.core.CoreApplicationEnvironment
-import com.intellij.lang.MetaLanguage
 import com.intellij.mock.MockProject
 import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.extensions.Extensions
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.psi.PsiManager
 import com.intellij.rt.execution.junit.FileComparisonFailure
-import com.intellij.uast.UastMetaLanguage
 import junit.framework.TestCase
 import org.jetbrains.uast.UastContext
 import org.jetbrains.uast.UastLanguagePlugin
@@ -63,11 +61,6 @@ abstract class AbstractTestWithCoreEnvironment : TestCase() {
 
         CoreApplicationEnvironment.registerExtensionPoint(
                 Extensions.getRootArea(),
-                MetaLanguage.EP_NAME,
-                MetaLanguage::class.java)
-
-        CoreApplicationEnvironment.registerExtensionPoint(
-                Extensions.getRootArea(),
                 UastLanguagePlugin.extensionPointName,
                 UastLanguagePlugin::class.java)
 
@@ -83,9 +76,6 @@ abstract class AbstractTestWithCoreEnvironment : TestCase() {
 
     private fun registerUastLanguagePlugins() {
         val area = Extensions.getRootArea()
-
-        area.getExtensionPoint(MetaLanguage.EP_NAME)
-                .registerExtension(UastMetaLanguage::class.java.newInstance())
 
         area.getExtensionPoint(UastLanguagePlugin.extensionPointName)
                 .registerExtension(JavaUastLanguagePlugin())
